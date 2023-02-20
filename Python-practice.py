31ff8e36da7f8feeb809bc0baa6440dfd91bb675
Orkata <50708833+Orkata63@users.noreply.github.com>
2023-02-20 13:50:33 +0100

    Add files via upload

---
+++ Python-practice.py
@@ -0,0 +1,40 @@
+"""
+print("hello world"
+      "bye world")
+
+if 5 > 2:
+    print("five is greater than two!")
+if 4 > 2:
+    print('four is greater than two')
+
+x = 5
+y = "hello"
+#commetar
+"""
+x = 2 #int
+y = "John" #string
+print(x)
+print(y)
+x = int(3)
+y = str(3)
+z = float(3)
+print(type(x))
+X=str(33)
+print(x)
+print(X)
+#variables
+myVarName = "john"
+MyVarName = "Rick"
+my_var_name = "sara"
+
+a,s,d = "Apple","peach","grappe"
+print(a)
+print(s)
+print(d)
+
+fruits = [ "Apple","peach","grappe" ] #collection
+q,w,e=fruits
+print(q)
+print(w)
+print(e)
+
